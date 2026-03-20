137870cad4c9eeded053e90042de84c28572f728
Christian Richter <christianrichter@seznam.cz>
2026-03-20 12:12:48 +0100

    Updates

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // Workout App Service Worker
 // Version + image list injected at build time
-const CACHE = 'workout-app-v1774004730542'
+const CACHE = 'workout-app-v1774005167565'
 const PRECACHE_IMAGES = ["/workout/assets/assets/Workout%20Images/Bench_Press.03728d4d81670930c321ddf5bf256684.png","/workout/assets/assets/Workout%20Images/Incline_Dumbbell_Press.5b09e27a8753da0beb082ed7531535e9.png","/workout/assets/assets/Workout%20Images/Lateral_Raises.88348c3e6f768d0afe474803d4fb75ca.png","/workout/assets/assets/Workout%20Images/Seated_Shoulder_Press.3eb40a8b2cc2a4ade09e699c363b2274.png","/workout/assets/assets/Workout%20Images/Triceps_Pushdown.dc66a790b42720fd47728128b332f111.png"]
 
 // Install: pre-cache all workout images immediately
